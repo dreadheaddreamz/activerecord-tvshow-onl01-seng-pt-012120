e6e7abe4dd54b6976461273384d02d8da6253656
James Batsell <jamesbatsell05@gmail.com>
2020-04-09 21:21:49 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |j|
+      j.string :name
+      j.string :network
+      j.string :day
+      j.integer :rating
+    end
+  end
+end
