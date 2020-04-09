ae3201f7c4a7bb278de27535c5ba19f156d2f8e1
James Batsell <jamesbatsell05@gmail.com>
2020-04-09 21:25:32 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,10 +1,6 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :shows do |j|
-      j.string :name
-      j.string :network
-      j.string :day
-      j.integer :rating
+    add_column
     end
   end
 end
